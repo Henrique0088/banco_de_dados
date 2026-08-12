f41ee2dd256d326edb3cc413337b7c8619ee7bd4
Henrique0088 <henriquerezendefurtado@gmail.com>
2026-08-12 16:45:48 -0300

    Add files via upload

---
+++ Aula 2/SQL_Exercicios_10.sql
@@ -0,0 +1,29 @@
+#Exercicio 1
+select nome from clientes order by nome asc;
+
+#Exercicio 2
+select nome from clientes order by nome desc;
+
+#Exercicio 3 
+select idade from clientes order by idade asc;
+
+#Exercicio 4
+select idade from clientes order by idade desc;
+
+#Exercicio 5
+select * from clientes limit 5;
+
+#Exercicio 6
+select idade, nome from clientes order by idade desc limit 3;
+
+#Exercicio 7 
+select nome, idade from clientes order by idade asc limit 2;
+
+#Exercicio 8
+select  distinct cidade from clientes order by cidade asc ;
+
+#Exercicio 9
+select * from clientes order by cidade asc limit 10;
+
+#Exercicio 10
+select nome, cidade, idade from clientes where cidade = 'Rio de janeiro' order by idade desc limit 5 ;
